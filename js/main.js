be2b9acffa30af8879c8718945dabcdb25178ff4
vladd111-source <help.implant@gmail.com>
2025-03-31 20:40:37 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -394,4 +394,3 @@ document.getElementById("placeForm")?.addEventListener("submit", (e) => {
     const duration = Math.round((Date.now() - appStart) / 1000);
     logEventToAnalytics("Сессия завершена", { duration_seconds: duration });
   });
-});
