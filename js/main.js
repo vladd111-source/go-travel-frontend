a8b5d3bf50819d7e326bba6af63aade51ab42427
vladd111-source <help.implant@gmail.com>
2025-04-01 07:03:07 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-console.log("✅ main.js загружен");
 // ✅ Supabase через CDN
 const supabaseUrl = 'https://hubrgeitdvodttderspj.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1YnJnZWl0ZHZvZHR0ZGVyc3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzY0OTEsImV4cCI6MjA1ODc1MjQ5MX0.K44XhDzjOodHzgl_cx80taX8Vgg_thFAVEesZUvKNnA';
