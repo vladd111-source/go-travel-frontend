346c4be73894cfee802d16d01643b326e4189f09
vladd111-source <help.implant@gmail.com>
2025-04-08 20:19:43 +0200

    Update globals.js

--- public/globals.js
+++ public/globals.js
@@ -167,7 +167,6 @@ window.switchFavTab = function(subTab) {
 
   // Запоминаем выбор
   localStorage.setItem("activeFavTab", subTab);
-};
 
 function capitalize(str) {
   return str.charAt(0).toUpperCase() + str.slice(1);
