6689db11f5fb3080a44fa2466a425f51374930a5
vladd111-source <help.implant@gmail.com>
2025-04-15 17:32:51 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -582,6 +582,7 @@ function toggleFavoritePlaceFromEncoded(encoded, element) {
     place: place.name,
   });
 }
+    });
     // Лог сессии
     window.addEventListener("beforeunload", () => {
       const duration = Math.round((Date.now() - window.appStart) / 1000);
