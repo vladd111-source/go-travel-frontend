f5c325329a92a900ef41b24455ebb116b418bcb8
vladd111-source <help.implant@gmail.com>
2025-04-19 02:20:24 +0200

    Update globals.js

--- public/globals.js
+++ public/globals.js
@@ -1,5 +1,5 @@
 // ❌ ничего не импортируем — просто используем window.generateAviasalesLink()
-const link = window.generateAviasalesLink(flight);
+import { renderFlights, renderHotels, renderPlaces } from './render.js';
 // ✅ Supabase init
 const supabaseUrl = 'https://hubrgeitdvodttderspj.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1YnJnZWl0ZHZvZHR0ZGVyc3BqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMxNzY0OTEsImV4cCI6MjA1ODc1MjQ5MX0.K44XhDzjOodHzgl_cx80taX8Vgg_thFAVEesZUvKNnA';
