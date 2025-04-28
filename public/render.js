b589ad7378a638c781d4efa06bd29c06264bbb74
vladd111-source <help.implant@gmail.com>
2025-04-28 20:27:49 +0200

    Update render.js

--- public/render.js
+++ public/render.js
@@ -210,7 +210,7 @@ export function renderHotels(hotels) {
 
  hotels.forEach(hotel => {
   const card = document.createElement("div");
- card.className = "card bg-white p-4 rounded-xl shadow mb-4";
+card.className = "card bg-white p-4 rounded-xl shadow mb-4 opacity-0 scale-95 transition-all duration-300";
 
   const hotelId = hotel.hotelId || hotel.id;
   const imageUrl = hotel.image 
